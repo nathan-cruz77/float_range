a87d475e88d8f83316084324d5df8deb37362478
Nathan Cruz <nathan.cruz@unifesp.br>
2017-01-07 18:24:02 -0200

    Pypi configuration files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'float_range',
+    packages = ['float_range'],
+    version = '0.1',
+    description = 'A range-compatible float number range generator',
+    author = 'Nathan Cruz',
+    url = 'https://github.com/nathan-cruz77/float_range',
+    download_url = 'https://github.com/nathan-cruz77/float_range/tarball/0.1',
+    keywords = ['range', 'generator', 'float', 'float_range'],
+    classifiers = []
+)
